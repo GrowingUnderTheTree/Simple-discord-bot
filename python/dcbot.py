75be9ced596b3b217f193c7625d08303e646751c
Young <81169774+GrowingUnderTheTree@users.noreply.github.com>
2022-04-03 18:39:33 +0800

    Making him not so controversial

--- python/dcbot.py
+++ python/dcbot.py
@@ -1,8 +1,9 @@
 from email import message
-from importlib.machinery import WindowsRegistryFinder
 import random
 import re
+from tkinter import N
 from tracemalloc import start
+from turtle import color, title
 import discord
 import time
 from discord.ext.commands import Bot
@@ -17,11 +18,13 @@ intents.presences = False
 intents.members = True
 bot = commands.Bot(command_prefix='#')
 
-TOKEN = "yes"
+TOKEN = "OTU4NzE0MDQ4ODUyMTM1OTM2.YkRWCQ._sxIQ5DUCWogCh1fORnPDruKg_s"
 client = discord.Client(intents=intents)
 intents = discord.Intents.all()
 game = discord.Game(name="Did you just told someone to shut?")
 notallowed = ['discrod.co','discrod.gift','gift.discord','nitro.discord','discord.nitro','dlscord']
+suicidal = ['depressed','kill','suicide','die','kys']
+lgbtq = ['furry','gay','lessbian','bisexual','lgbtq','furry','transgender']
 
 
 @client.event
@@ -43,11 +46,9 @@ async def on_message(message):
     now = datetime.now()
     currenttime = now.strftime("%H:%M:%S")
     warn = ['Oh no league players', 'Someone kick him please', 'mods ban him', '💀']
-    gay = ['https://tenor.com/view/4k-caught-caught-in4k-caught-in8k-8k-gif-20014426','https://tenor.com/view/skeleton-trumpet-gif-4622525','https://tenor.com/view/skeleton-dance-gif-23861627']
     l1984 = ['Literally 1984','Big brother is watching you']
     pp = ['round','hexagon','double barell','Dont have','curved','short']
     size = random.choice(pp)
-    gays = random.choice(gay)
     warns = random.choice(warn)
     literally = random.choice(l1984)
     if 'i love league of legends' in message.content.lower():
@@ -55,13 +56,11 @@ async def on_message(message):
     elif 'segs' in message.content.lower():
         await message.channel.send('No.')
     elif 'i am a furry' in message.content.lower():
-        await message.channel.send('https://letmegooglethat.com/?q=psycholgical+therapy+near+me')
+        await message.channel.send('discord.gg/pride')
     elif 'french house music sucks' in message.content.lower():
         await message.channel.send('ip pulled')
     elif '<3' in message.content.lower():
         await message.channel.send('!!!')
-    elif 'i am gay' in message.content.lower():
-        await message.channel.send(gays)
     elif 'nig' in message.content.lower():
         await message.channel.send('https://tenor.com/view/hog-rider-coc-clash-of-clans-gif-24053135')
     elif '?warn' in message.content.lower():
@@ -76,10 +75,19 @@ async def on_message(message):
         await message.channel.send('Bro just change to dnd')
         await client.change_presence(status=discord.Status.idle, activity=game)
     elif any(words in message.content.lower() for words in notallowed):
-            embedVar = discord.Embed(title="Are you sending on the wrong link?", description="Hi, it looks like the link you sent is a scam", color=0x00ff00)
+            embedVar = discord.Embed(title="Are you sending the wrong link?", description="Hi, it looks like the link you sent is a scam", color=0x00ff00)
             embedVar.add_field(name="Why?", value="The link you sent here is not discord.com, but something else. So, please look closely to check whether is there a typo.", inline=False)
             embedVar.add_field(name="Be careful", value="There's no free things in the world, so beware everytime if you are going to click a Nitro gift link.\n p.s. follow me at github http://github.com/GrowingUnderTheTree", inline=False)
             await message.channel.send(embed=embedVar)
+    elif any(suicidalwords in message.content.lower() for suicidalwords in suicidal):
+            embedstuff = discord.Embed(title="Need help?", description="Killing yourself can't solve the problems.\n Please take a look at the helplist below", color=0xB00B69)
+            embedstuff.add_field(name="Websites if you need help", value="Safe in our world : https://safeinourworld.org\nBefrienders : https://www.befrienders.org.my\nLifeline : https://suicidepreventionlifeline.org", inline=False)
+            embedstuff.add_field(name="Hotlines", value="03-76272929 for Malaysia", inline=False)
+            await message.channel.send(embed=embedstuff)
+    elif any(pride in message.content.lower() for pride in lgbtq):
+        embedlgbt = discord.Embed(title="LGBTQ+ yes", description = "discord.gg/pride", color = 0xB00B69)
+        embedlgbt.add_field(name="Learn more here",value="https://en.wikipedia.org/wiki/LGBT", inline=False)
+        await message.channel.send(embed=embedlgbt)
 
 """
 @client.event
@@ -94,3 +102,4 @@ async def on_message(message):
 client.run(TOKEN)
 end = time.time()
 print('Code stopped after ', end-start, ' Seconds')
+
